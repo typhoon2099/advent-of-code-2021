58e9f175dff067d6182450386926676148686223
Matthew Simpson <matthew@matthewsimpson.net>
2022-03-23 21:19:36 +0000

    Add Day 2, Part 2

--- src/day1.rs
+++ src/day1.rs
@@ -1,15 +1,26 @@
 pub fn part1() -> usize {
-    let raw_depths = String::from_utf8(include_bytes!("../data/day1.txt").to_vec()).unwrap();
-    let depth_strings: Vec<&str> = raw_depths.split("\n").collect();
+    depths()
+        .windows(2)
+        .filter(|window| window[1] > window[0])
+        .count()
+}
 
-    let depths: Vec<usize> = depth_strings
+pub fn part2() -> usize {
+    depths()
+        .windows(3)
+        .map(|window| window[0] + window[1] + window[2])
+        .collect::<Vec<usize>>()
+        .windows(2)
+        .filter(|window| window[1] > window[0] )
+        .count()
+}
+
+fn depths() -> Vec<usize> {
+    let raw_depths = String::from_utf8(include_bytes!("../data/day1.txt").to_vec()).unwrap();
+    let depth_strings: Vec<&str> = raw_depths.split_whitespace().collect();
+
+    depth_strings
         .iter()
         .map(|x| x.parse::<usize>().unwrap_or(0))
-        .collect();
-
-    let windows = depths.windows(2);
-
-    windows.filter(|window|
-        window[1] > window[0]
-    ).count()
+        .collect()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 mod day1;
 
 fn main() {
-    println!("Day 1: {}", day1::part1());
+    println!("Day 1: {} and {}", day1::part1(), day1::part2());
 }
